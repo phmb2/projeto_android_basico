2169002431d5717e0c74b68a5cea02fe0d7853d8
phmb2 <phmb002@gmail.com>
2021-10-20 17:34:08 -0300

    Adding three unit tests

---
+++ app/src/test/java/com/phmb/projeto_android_basico/MovieUnitTest.kt
@@ -0,0 +1,34 @@
+package com.phmb.projeto_android_basico
+
+import com.phmb.projeto_android_basico.model.Movie
+import org.junit.Test
+
+class MovieUnitTest {
+
+    private var moviesArrayList = arrayListOf<Movie>()
+
+    @Test
+    fun `When adding a movie the list size should increase by two`() {
+        val oldSize =  moviesArrayList.size
+        moviesArrayList.add(Movie("Movie 1", "14/07/2010", "Fulano", 1))
+        moviesArrayList.add(Movie("Movie 2", "10/10/2015", "Sicrano", 2))
+        val newSize = moviesArrayList.size
+        assert(newSize == (oldSize+2))
+    }
+
+    @Test
+    fun `When an movie is added, it should be inside the movies list`() {
+        val newMovie = Movie("Movie 1", "14/07/2010", "Fulano", 1)
+        moviesArrayList.add(newMovie)
+        assert(moviesArrayList.contains(newMovie))
+    }
+
+    @Test
+    fun `When an movie is removed, it should not be inside the movies list`() {
+        val newMovie = Movie("Movie 2", "10/10/2015", "Sicrano", 2)
+        moviesArrayList.add(newMovie)
+        moviesArrayList.remove(newMovie)
+        assert(!moviesArrayList.contains(newMovie))
+    }
+
+}
